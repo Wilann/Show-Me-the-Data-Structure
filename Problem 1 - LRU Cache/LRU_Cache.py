67eef67016cedb6100e4cce743fe26f38d25fcef
Wilson Chan <wchan052@uottawa.ca>
2019-11-01 12:25:03 -0400

    Added comments for testing

--- Problem 1 - LRU Cache/LRU_Cache.py
+++ Problem 1 - LRU Cache/LRU_Cache.py
@@ -207,42 +207,43 @@ our_cache = LRU_Cache(5)
 print("our_cache:", our_cache, '\n')
 
 print("--our_cache.set(1, 1)--")
-our_cache.set(1, 1)
+our_cache.set(1, 1)  # No return
 print("our_cache:", our_cache, '\n')
 
 print("--our_cache.set(2, 2)--")
-our_cache.set(2, 2)
+our_cache.set(2, 2)  # No return
 print("our_cache:", our_cache, '\n')
 
 print("--our_cache.set(3, 3)--")
-our_cache.set(3, 3)
+our_cache.set(3, 3)  # No return
 print("our_cache:", our_cache, '\n')
 
 print("--our_cache.set(4, 4)--")
-our_cache.set(4, 4)
+our_cache.set(4, 4)  # No return
 print("our_cache:", our_cache, '\n')
 
 print("--our_cache.get(1)--")
-print(our_cache.get(1))  # returns 1
+print("our_cache.get(1):", our_cache.get(1))  # returns 1
 print("our_cache:", our_cache, '\n')
 
 print("--our_cache.get(2)--")
-print(our_cache.get(2))  # returns 2
+print("our_cache.get(2):", our_cache.get(2))  # returns 2
 print("our_cache:", our_cache, '\n')
 
 print("--our_cache.get(9)--")
-print(our_cache.get(9))  # returns -1 because 9 is not present in the cache
+print("our_cache.get(9):", our_cache.get(9))  # returns -1 because 9 is not present in the cache
 print("our_cache:", our_cache, '\n')
 
 print("--our_cache.set(5, 5)--")
-our_cache.set(5, 5)
+our_cache.set(5, 5)  # No return
 print("our_cache:", our_cache, '\n')
 
 print("--our_cache.set(6, 6)--")
-our_cache.set(6, 6)
+our_cache.set(6, 6)  # No return
 print("our_cache:", our_cache, '\n')
 
-# returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+
 print("--our_cache.get(3)--")
-print(our_cache.get(3))
+print("our_cache.get(3):", our_cache.get(3))
+# returns -1 because the cache reached it's capacity and 3 was the least recently used entry
 print("our_cache:", our_cache, '\n')
